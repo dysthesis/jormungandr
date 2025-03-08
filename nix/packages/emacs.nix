60fddfabab5d0d472ffc1c514372fc39215430b8
Dysthesis <antheoraviel@protonmail.com>
2025-03-09 09:45:13 +1100

    Try to fix this

--- nix/packages/emacs.nix
+++ nix/packages/emacs.nix
@@ -1,19 +1,37 @@
 {
   emacs-unstable,
-  writeText,
   emacsWithPackagesFromUsePackage,
+  runCommand,
 }: let
+  emacs = emacs-unstable;
   config =
-    writeText "init.el"
-    /*
-    emacs-lisp
-    */
-    ''
-      (org-babel-load-file "${../../README.org}")
-      (add-to-list 'custom-theme-load-path "${../../themes}")
+    runCommand "init.el" {
+      buildInputs = [emacs];
+      src = ../../README.org;
+      resultIsFile = true;
+    } ''
+         # Tangle the Org file using Emacs in batch mode.
+      cp $src init.org
+      emacs --batch \
+       -l org init.org \
+       -f org-babel-tangle
+
+
+      echo "(add-to-list 'custom-theme-load-path "${../../themes}")" > $out
+      cat init.el >> $out
     '';
+  # config =
+  #   writeText "init.el"
+  #   /*
+  #   emacs-lisp
+  #   */
+  #   ''
+  #     (add-to-list 'custom-theme-load-path "${../../themes}")
+  #     (org-babel-load-file "${../../README.org}")
+  #   '';
 in
   emacsWithPackagesFromUsePackage {
     inherit config;
+    package = emacs;
     alwaysEnsure = true;
   }
