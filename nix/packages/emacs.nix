a535b34630d75bc77dc1e579e0423cd9a1aa20f5
Dysthesis <antheoraviel@protonmail.com>
2025-03-13 10:20:02 +1100

    Changes

--- nix/packages/emacs.nix
+++ nix/packages/emacs.nix
@@ -3,8 +3,33 @@
   lib,
   emacs-unstable,
   emacsWithPackagesFromUsePackage,
+  runCommand,
 }: let
   inherit (lib.babel.pkgs) mkWrapper;
+  inherit (lib.babel.emacs) orgTangle;
+  orgFile = ../../README.org;
+  initFile = orgTangle pkgs emacs-unstable orgFile "init.org";
+
+  cfg =
+    runCommand "init.el" {
+      buildInputs = [emacs-unstable];
+      src = "${initFile}/output.el";
+    } ''
+
+
+      mkdir -p $out
+      mkdir -p $out/eln-cache
+      # echo "(setq native-comp-eln-load-path (list \"$out/eln-cache\"))" >> $out/init.el
+      echo "(add-to-list 'native-comp-eln-load-path \"$out/eln-cache\")" >> $out/init.el
+      echo "(add-to-list 'native-comp-eln-load-path \"$emacs/share/emacs/native-lisp\")" >> $out/init.el
+      cat $src >> $out/init.el
+
+
+      emacs --batch \
+      	--eval "(setq native-comp-eln-load-path (list \"$out/eln-cache\"))" \
+      	--eval "(native-compile \"$out/init.el\")"
+    '';
+
   package = emacs-unstable.overrideAttrs (old: {
     postInstall =
       /*
@@ -14,26 +39,16 @@
         ${old.postInstall or ""}
         shopt -s globstar
         ln -sf $out/share/emacs/native-lisp/*/* $out/lib/emacs/*/native-lisp/*/
+        ln -sf ${cfg}/eln-cache/* $out/lib/emacs/*/native-lisp/*/
         shopt -u globstar
       '';
   });
 
-  config = ../../README.org;
-
-  wrapped-emacs = emacsWithPackagesFromUsePackage {
+  config = "${cfg}/init.el";
+in
+  emacsWithPackagesFromUsePackage {
     inherit package config;
     alwaysEnsure = true;
     alwaysTangle = true;
     defaultInitFile = true;
-  };
-in
-  wrapped-emacs
-# mkWrapper pkgs wrapped-emacs
-# /*
-# bash
-# */
-# ''
-#   wrapProgram $out/bin/emacs \
-#    --prefix "EMACSNATIVELOADPATH" ":" "${package}/share/emacs/native-lisp/"
-# ''
-
+  }
