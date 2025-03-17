cbc83e8656875e9c80c1d03d6ff290f0e2927e51
Dysthesis <antheoraviel@protonmail.com>
2025-03-17 18:42:15 +1100

    No clown fiesta theme works

--- nix/packages/noClownFiesta.nix
+++ nix/packages/noClownFiesta.nix
@@ -2,12 +2,13 @@
   pkgs,
   emacs-unstable,
   ...
-}: let
-in
-  pkgs.runCommand "no-clown-fiesta" {
-    nativeBuildInputs = [emacs-unstable];
+}:
+pkgs.runCommand "no-clown-fiesta"
+  {
+    nativeBuildInputs = [ emacs-unstable ];
     src = ../../themes/no-clown-fiesta.el;
-  } ''
+  }
+  ''
     mkdir -p $out/share/emacs/site-lisp
-    cp $src $out/share/emacs/site-lisp/no-clown-fiesta.el
+    cp $src $out/share/emacs/site-lisp/no-clown-fiesta-theme.el
   ''
