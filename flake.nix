d2f5bacc71b24b0090a014580addae33d36808d6
Dysthesis <antheoraviel@protonmail.com>
2026-02-26 14:42:59 +1100

    Fix some clanker mistake

--- flake.nix
+++ flake.nix
@@ -32,48 +32,39 @@
             inherit pkgs;
             p = epkgs;
           };
+
         tangle = pkgs.callPackage ./nix/tangle.nix {};
-        emacs-unwrapped = pkgs.emacsWithPackagesFromUsePackage {
+        jormungandr-unwrapped = pkgs.emacsWithPackagesFromUsePackage {
           package = pkgs.emacs-unstable-pgtk;
           config = "${tangle}/init.el";
+          defaultInitFile = true;
           # Extra packages that aren't discoverable via :ensure or need pinning.
           extraEmacsPackages = epkgs:
-            (vcPackagesFor epkgs) ++ (with pkgs; [
+            (vcPackagesFor epkgs)
+            ++ (with pkgs; [
               epkgs.nerd-icons
               emacs-lsp-booster
             ]);
         };
-        bootScript = pkgs.writeShellScript "jormungandr-boot" ''
-          set -e
-          cfgroot=''${XDG_CONFIG_HOME:-$HOME/.config}/jormungandr
-          export XDG_CONFIG_HOME="$cfgroot"
-          cfg="$cfgroot/emacs"
-          if [ ! -e "$cfg/init.el" ]; then
-            mkdir -p "$cfg"
-            cp -r ${tangle}/share/jormungandr/* "$cfg"/
-          fi
-          set -- "--init-directory" "$cfg" \
-                 "--load" "$cfg/early-init.el" \
-                 "--load" "$cfg/init.el" \
-                 "$@"
-        '';
 
         jormungandr = pkgs.symlinkJoin {
           name = "jormungandr";
-          paths = [emacs-unwrapped];
-          buildInputs = [pkgs.makeWrapper];
+          paths = [jormungandr-unwrapped];
+          nativeBuildInputs = [pkgs.makeWrapper];
           postBuild = ''
-            rm $out/bin/emacs
-            makeWrapper ${emacs-unwrapped}/bin/emacs $out/bin/emacs \
-              --set JORMUNGANDR_DISABLE_PACKAGE_EL 1 \
-              --run ". ${bootScript}"
+            wrapProgram $out/bin/emacs \
+              --set JORMUNGANDR_DISABLE_PACKAGE_EL 1
           '';
         };
       in {
         _module.args.pkgs = pkgs;
 
         packages = {
-          inherit tangle jormungandr;
+          inherit
+            tangle
+            jormungandr
+            jormungandr-unwrapped
+            ;
         };
 
         apps.default = {
